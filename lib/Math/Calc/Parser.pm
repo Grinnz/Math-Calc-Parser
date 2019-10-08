718eb38e15d19f1ebe81d063a6f3c83a5de55bab
Dan Book <grinnz@grinnz.com>
2019-10-08 17:39:29 -0400

    Bump version

--- lib/Math/Calc/Parser.pm
+++ lib/Math/Calc/Parser.pm
@@ -8,7 +8,7 @@ use Math::Complex ();
 use POSIX ();
 use Scalar::Util ();
 
-our $VERSION = '1.005';
+our $VERSION = '1.006';
 our @ISA = 'Exporter';
 our @EXPORT_OK = 'calc';
 our $ERROR;
--- lib/ath.pm
+++ lib/ath.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Math::Calc::Parser;
 use Encode ();
-our $VERSION = '1.005';
+our $VERSION = '1.006';
 my ($bignum, $bigrat);
 sub import {
 	my $class = shift;
